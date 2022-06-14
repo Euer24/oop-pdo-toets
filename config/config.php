160ab146737904caed98baaa3946c1d1e572548c
Euer <43913273+Euer24@users.noreply.github.com>
2022-06-14 10:25:16 +0200

    2e commit

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Hieronder staan de gegevens van de gebruiker in de database
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'oop-pdo-toets');
+
+
+
+
+
+
+
+
+?>
